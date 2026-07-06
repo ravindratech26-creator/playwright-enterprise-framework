53fe281262290c085edcefa2c088b602d1bf7b82
Ravindra <ravindratech26@gmail.com>
2026-07-06 22:59:29 +0530

    Add custom Playwright fixtures for page objects

---
+++ fixtures/pages.ts
@@ -0,0 +1,22 @@
+import { test as base } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+import { InventoryPage } from '../pages/InventoryPage';
+
+type MyFixtures = {
+    loginPage: LoginPage;
+    inventoryPage: InventoryPage;
+};
+
+export const test = base.extend<MyFixtures>({
+
+    loginPage: async ({ page }, use) => {
+        await use(new LoginPage(page));
+    },
+
+    inventoryPage: async ({ page }, use) => {
+        await use(new InventoryPage(page));
+    }
+
+});
+
+export { expect } from '@playwright/test';
--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,26 +1,17 @@
-import { expect, test } from '@playwright/test';
-import { LoginPage } from '../../pages/LoginPage';
-import { InventoryPage } from '../../pages/InventoryPage';
+import { test, expect } from '../../fixtures/pages';
 import { loginData } from '../../test-data/loginData';
 
-
-test('Valid Login', async ({ page }) => {
-
-    // Arrange
-    const loginPage = new LoginPage(page);
-    const inventoryPage = new InventoryPage(page);
+test('Verify user can login with valid credentials', async ({ page, loginPage, inventoryPage }) => {
 
     await page.goto('/');
 
-    // Act
-    //await loginPage.login('standard_user', 'secret_sauce');
     await loginPage.login(
-    loginData.validUser.username,
-    loginData.validUser.password
+        loginData.validUser.username,
+        loginData.validUser.password
     );
 
-    // Assert
     await expect(page).toHaveURL('/inventory.html');
+
     await inventoryPage.isInventoryPageDisplayed();
 
 });
