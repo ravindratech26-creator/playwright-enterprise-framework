cb115f377565d2e980dbed1fbc71d2932c5e1688
Ravindra <ravindratech26@gmail.com>
2026-07-06 22:09:31 +0530

    Configure base URL in Playwright config

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -5,15 +5,17 @@ test('Valid Login', async ({page})=>{
     const loginPage=new LoginPage(page);
 
     // Navigate to application
-    await page.goto('https://www.saucedemo.com');
+    //await page.goto('https://www.saucedemo.com');
+    await page.goto('/');
 
     // Act
 
     await loginPage.login('standard_user', 'secret_sauce');
 
     //Assert
-    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-    
+    //await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    await expect(page).toHaveURL('/inventory.html');
+
     
 
     
