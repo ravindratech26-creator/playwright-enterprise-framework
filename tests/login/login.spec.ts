10b1e7fc525cf72d1d05ed4483c21e7115135533
Ravindra <ravindratech26@gmail.com>
2026-07-06 21:51:25 +0530

    Implement valid login test flow

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,6 +1,23 @@
 import {test, expect} from '@playwright/test';
 import { LoginPage } from '../../pages/LoginPage';
 test('Valid Login', async ({page})=>{
+    // Arrange
+    const loginPage=new LoginPage(page);
+
+    // Navigate to application
+    await page.goto('https://www.saucedemo.com');
+
+    // Act
+
+    await loginPage.login('standard_user', 'secret_sauce');
+
+    //Assert
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    
+    
+
+    
+    
     
 
 });
