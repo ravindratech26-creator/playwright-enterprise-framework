ea27e4198ce65b141e2466f1011fe8abf2e027b1
Ravindra <ravindratech26@gmail.com>
2026-07-06 22:30:56 +0530

    Move login credentials to test data

---
+++ test-data/loginData.ts
@@ -0,0 +1,6 @@
+export const loginData = {
+    validUser: {
+        username: 'standard_user',
+        password: 'secret_sauce'
+    }
+};
--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,6 +1,8 @@
 import { expect, test } from '@playwright/test';
 import { LoginPage } from '../../pages/LoginPage';
 import { InventoryPage } from '../../pages/InventoryPage';
+import { loginData } from '../../test-data/loginData';
+
 
 test('Valid Login', async ({ page }) => {
 
@@ -11,7 +13,11 @@ test('Valid Login', async ({ page }) => {
     await page.goto('/');
 
     // Act
-    await loginPage.login('standard_user', 'secret_sauce');
+    //await loginPage.login('standard_user', 'secret_sauce');
+    await loginPage.login(
+    loginData.validUser.username,
+    loginData.validUser.password
+    );
 
     // Assert
     await expect(page).toHaveURL('/inventory.html');
