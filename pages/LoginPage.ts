7e0e53c5b28a84241b4d91356561f4b364eb32c4
Ravindra <ravindratech26@gmail.com>
2026-07-06 21:30:09 +0530

    Complete login business method

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -18,7 +18,9 @@ export class LoginPage {
 
     async login(username: string, password: string){
         await this.usernameInput.fill(username);
-        
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+
 
 
     }
